ca58fd8e9ae1e9c6043b6d88e79d425c874f74a4
jgpark-1 <jgpark7281@gmail.com>
2024-01-13 06:39:23 +0900

    feat: add sub category page, layout

--- src/app/examples/nextjs/routing/[slug]/[category]/page.tsx
+++ src/app/examples/nextjs/routing/[slug]/[category]/page.tsx
@@ -5,5 +5,5 @@ import { useParams } from "next/navigation";
 export default function Page() {
   const params = useParams();
 
-  return <div className="p-4 border border-dashed border-inherit">{params.category}</div>;
+  return <div className="">{params.category}</div>;
 }
