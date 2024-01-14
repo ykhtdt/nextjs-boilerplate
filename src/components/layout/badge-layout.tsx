29fa5987d091259a29f6b87c0fafa76be48c5c13
jgpark-1 <jgpark7281@gmail.com>
2024-01-15 07:48:15 +0900

    feat: add components

---
+++ src/components/layout/badge-layout.tsx
@@ -0,0 +1,22 @@
+import { Badge } from "@/components/ui/badge"
+
+interface Props {
+  display: string;
+  children: React.ReactNode;
+}
+
+export function BadgeLayout({ display, children }: Props) {
+  return (
+    <div className="relative px-4 py-6 border border-zinc-800">
+      <Badge
+        variant="secondary"
+        className="absolute font-normal uppercase tracking-widest -top-2.5 bg-zinc-800 text-[10px]"
+      >
+        {display}
+      </Badge>
+      <div className="space-y-6">
+        {children}
+      </div>
+    </div>
+  );
+}
