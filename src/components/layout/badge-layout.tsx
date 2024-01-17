59d37437bd5e5a066c8499363b8f02229b96bba9
jgpark-1 <jgpark7281@gmail.com>
2024-01-17 17:20:31 +0900

    feat: modify badge layout padding

--- src/components/layout/badge-layout.tsx
+++ src/components/layout/badge-layout.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export function BadgeLayout({ display, children }: Props) {
   return (
-    <div className="relative px-4 py-6 border border-zinc-800">
+    <div className="relative px-6 py-8 border border-zinc-800">
       <Badge variant="secondary" className="absolute font-normal uppercase tracking-widest -top-2.5 bg-zinc-800 text-[10px]">
         {display}
       </Badge>
