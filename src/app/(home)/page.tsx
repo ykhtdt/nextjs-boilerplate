9543f4585f81865a003d31f2339fac521bc9d5a0
jgpark-1 <jgpark7281@gmail.com>
2024-02-06 16:06:14 +0900

    feat: disabled docs page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
       <main id="content">
         <IntroSection />
         <FeatureSection />
-        <StartSection />
+        {/* <StartSection />  */}
       </main>
     </ScrollContainer>
   );
