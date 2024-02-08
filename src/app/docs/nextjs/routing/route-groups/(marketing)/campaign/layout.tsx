eded06ab1c2481243013601231b0b85eb7c9fc4a
jgpark-1 <jgpark7281@gmail.com>
2024-02-09 03:45:52 +0900

    feat: remove docs

--- src/app/docs/nextjs/routing/route-groups/(marketing)/campaign/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { BadgeLayout } from "@/components/layout/badge-layout";
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <BadgeLayout display="Campaign Layout">
-      <div className="px-2">{children}</div>
-    </BadgeLayout>
-  );
-}
