7b16645ccab9d58bdde3b2286236862961ec9296
jgpark-1 <jgpark7281@gmail.com>
2024-02-15 22:01:59 +0900

    feat: add clamp utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function clamp(value: number, min: number, max: number) {
+  if (value <= min) {
+    return min;
+  }
+
+  if (value >= max) {
+    return max;
+  }
+
+  return value;
+}
