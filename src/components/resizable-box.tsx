110806d453401381bd75db5655d2c624d7ef8144
jgpark-1 <jgpark7281@gmail.com>
2024-02-16 00:55:17 +0900

    chore(wip): develop resizeable-box component
    add resize event, add resize handler

--- src/components/resizable-box.tsx
+++ src/components/resizable-box.tsx
@@ -1,25 +1,33 @@
 "use client";
 
-import { PropsWithChildren, useCallback, useLayoutEffect, useRef, useState } from "react";
+import type { NonEmptyArray } from "@/types/common";
+
+import { useLayoutEffect, useRef, useState } from "react";
 
 import { cva } from "class-variance-authority";
-import { cn } from "@/lib/utils";
+import { cn, clamp } from "@/lib/utils";
 
 type ResizeHandleAxis = "n" | "ne" | "e" | "se" | "s" | "sw" | "w" | "nw";
 
 type Bounds = {
-  top?: number;
-  left?: number;
-  width?: number;
-  height?: number;
-}
+  top: number;
+  left: number;
+  width: number;
+  height: number;
+};
 
-type Props = {
+type ResizeEvent = {
+  onResizeStart?: (event: React.MouseEvent<Element, MouseEvent>) => void;
+  onResize?: (event: MouseEvent) => void;
+  onResizeStop?: (event: MouseEvent) => void;
+};
+
+type ResizableBoxProps = {
   className?: string;
-  resizeHandleAxis?: ResizeHandleAxis[];
+  resizeHandleAxis?: NonEmptyArray<ResizeHandleAxis>;
   maxConstraints?: [number, number];
   minConstraints?: [number, number];
-} & Bounds;
+} & Partial<Bounds> & ResizeEvent;
 
 export default function ResizableBox({
   children,
@@ -31,38 +39,19 @@ export default function ResizableBox({
   height,
   maxConstraints = [Infinity, Infinity],
   minConstraints = [16, 16],
-}: PropsWithChildren<Props>) {
-  const [bounds, setBounds] = useState<Bounds>({ top, left, width, height });
+  onResizeStart,
+  onResize,
+  onResizeStop,
+}: React.PropsWithChildren<ResizableBoxProps>) {
+  const boundsProps = { top, left, width, height };
 
-  const resizeRegister = (onDragChange: (deltaX: number, deltaY: number) => void) => {
-    return {
-      onMouseDown: (clickEvent: React.MouseEvent<Element, MouseEvent>) => {
-        clickEvent.stopPropagation();
-        clickEvent.preventDefault();
+  const filteredBoundsProps = Object.fromEntries(Object.entries(boundsProps).filter(([key, value]) => value !== undefined)) as Bounds;
 
-        const handleMouseMove = (moveEvent: MouseEvent) => {
-          const deltaX = moveEvent.pageX - clickEvent.pageX;
-          const deltaY = moveEvent.pageY - clickEvent.pageY;
-          onDragChange(deltaX, deltaY);
-        };
-
-        const handleMouseUp = () => {
-          document.removeEventListener("mousemove", handleMouseMove);
-        };
-
-        document.addEventListener("mousemove", handleMouseMove);
-        document.addEventListener("mouseup", handleMouseUp, { once: true });
-      },
-    };
-  };
+  const [bounds, setBounds] = useState<Bounds>(filteredBoundsProps);
 
   const boundaryRef = useRef<HTMLDivElement>(null);
 
   useLayoutEffect(() => {
-    // if (width && height && x && y) {
-    //   return;
-    // }
-
     if (boundaryRef.current) {
       const boundary = boundaryRef.current.getBoundingClientRect();
 
@@ -75,102 +64,138 @@ export default function ResizableBox({
     }
   }, []);
 
-  const inRange = (value: number, min: number, max: number) => {
-    if (value <= min) {
-      return min;
-    }
-
-    if (value >= max) {
-      return max;
-    }
-
-    return value;
-  }
-
-  const generateResizeHandle = useCallback((axis: ResizeHandleAxis) => {
-    const calculateWidth = (width: number, deltaX: number) => {
-      if (axis.includes("e")) {
-        return inRange(width + deltaX, minConstraints[0], maxConstraints[0]);
-      }
-
-      if (axis.includes("w")) {
-        return inRange(width - deltaX, minConstraints[0], maxConstraints[0]);
-      }
-
-      return width;
-    };
-
-    const calculateHeight = (height: number, deltaY: number) => {
-      if (axis.includes("n")) {
-        return inRange(height - deltaY, minConstraints[1], maxConstraints[1]);
-      }
-
-      if (axis.includes("s")) {
-        return inRange(height + deltaY, minConstraints[1], maxConstraints[1]);
-      }
-
-      return height;
-    };
-
-    const calculateTop = (bounds: Bounds, deltaY: number) => {
-      if (bounds.top !== undefined && bounds.height !== undefined) {
-        if (axis.includes("n")) {
-          return inRange(bounds.top + deltaY, bounds.height + bounds.top - maxConstraints[1], bounds.top + bounds.height - minConstraints[1]);
-        }
-      }
-
-      return bounds.top;
-    }
-
-    const calculateLeft = (bounds: Bounds, deltaX: number) => {
-      if (bounds.left !== undefined && bounds.width !== undefined) {
-        if (axis.includes("w")) {
-          return inRange(bounds.left + deltaX, bounds.width + bounds.left - maxConstraints[0], bounds.left + bounds.width - minConstraints[0]);
-        }
-      }
-
-      return bounds.left;
-    }
-
-    const params = {
-      ...resizeRegister((deltaX, deltaY) => {
-        if (bounds.top === undefined || bounds.left === undefined || bounds.width === undefined || bounds.height === undefined) {
-          return;
-        }
-
-        const nextBounds = {
-          top: calculateTop(bounds, deltaY),
-          left: calculateLeft(bounds, deltaX),
-          width: calculateWidth(bounds.width, deltaX),
-          height: calculateHeight(bounds.height, deltaY),
-        };
-
-        setBounds({ ...nextBounds });
-      })
-    };
-
-    const resizeHandleVariants = cva("absolute", {
-      variants: {
-        axis: {
-          n: "top-0 left-0 translate-y-[-50%] w-full h-4 cursor-n-resize",
-          ne: "top-0 right-0 translate-x-[50%] translate-y-[-50%] w-4 h-4 cursor-ne-resize",
-          e: "top-0 right-0 translate-x-[50%] w-3 h-full cursor-e-resize",
-          se: "bottom-0 right-0 translate-x-[50%] translate-y-[50%] w-4 h-4 cursor-se-resize",
-          s: "bottom-0 left-0 translate-y-[50%] w-full h-3 cursor-s-resize",
-          sw: "bottom-0 left-0 translate-x-[-50%] translate-y-[50%] w-4 h-4 cursor-sw-resize",
-          w: "bottom-0 left-0 translate-x-[-50%] w-3 h-full cursor-w-resize",
-          nw: "top-0 left-0 translate-x-[-50%] translate-y-[-50%] w-4 h-4 cursor-nw-resize",
-        }
-      }
-    })
-
-    return <span key={axis} {...params} className={cn(resizeHandleVariants({ axis }))} />
-  }, [bounds, maxConstraints, minConstraints]);
-
   return (
     <div ref={boundaryRef} style={{ ...bounds }} className={cn(className, "relative")}>
       {children}
-      {resizeHandleAxis && resizeHandleAxis.map((axis) => generateResizeHandle(axis))}
+      {resizeHandleAxis &&
+        resizeHandleAxis.map((axis) => (
+          <ResizeHandler
+            key={axis}
+            axis={axis}
+            bounds={bounds}
+            maxConstraints={maxConstraints}
+            minConstraints={minConstraints}
+            setBounds={setBounds}
+            onResizeStart={onResizeStart}
+            onResize={onResize}
+            onResizeStop={onResizeStop}
+          />
+        ))}
     </div>
   );
+}
+
+const resizeHandleVariants = cva("absolute", {
+  variants: {
+    axis: {
+      n: "top-0 left-0 translate-y-[-50%] w-full h-4 cursor-n-resize",
+      ne: "top-0 right-0 translate-x-[50%] translate-y-[-50%] w-4 h-4 cursor-ne-resize",
+      e: "top-0 right-0 translate-x-[50%] w-3 h-full cursor-e-resize",
+      se: "bottom-0 right-0 translate-x-[50%] translate-y-[50%] w-4 h-4 cursor-se-resize",
+      s: "bottom-0 left-0 translate-y-[50%] w-full h-3 cursor-s-resize",
+      sw: "bottom-0 left-0 translate-x-[-50%] translate-y-[50%] w-4 h-4 cursor-sw-resize",
+      w: "bottom-0 left-0 translate-x-[-50%] w-3 h-full cursor-w-resize",
+      nw: "top-0 left-0 translate-x-[-50%] translate-y-[-50%] w-4 h-4 cursor-nw-resize",
+    },
+  },
+});
+
+type ResizeHandleProps = {
+  axis: ResizeHandleAxis;
+  maxConstraints: [number, number];
+  minConstraints: [number, number];
+  bounds: Bounds;
+  setBounds: React.Dispatch<React.SetStateAction<Bounds>>;
+} & ResizeEvent;
+
+function ResizeHandler({
+  axis,
+  maxConstraints,
+  minConstraints,
+  bounds,
+  setBounds,
+  onResizeStart,
+  onResize,
+  onResizeStop,
+}: ResizeHandleProps) {
+  const resizeRegister = (onDragChange: (deltaX: number, deltaY: number) => void) => {
+    return {
+      onMouseDown: (clickEvent: React.MouseEvent<Element, MouseEvent>) => {
+        clickEvent.stopPropagation();
+        clickEvent.preventDefault();
+
+        onResizeStart && onResizeStart(clickEvent);
+
+        const handleMouseMove = (moveEvent: MouseEvent) => {
+          const deltaX = moveEvent.pageX - clickEvent.pageX;
+          const deltaY = moveEvent.pageY - clickEvent.pageY;
+          onDragChange(deltaX, deltaY);
+          onResize && onResize(moveEvent);
+        };
+
+        const handleMouseUp = (upEvent: MouseEvent) => {
+          onResizeStop && onResizeStop(upEvent);
+          document.removeEventListener("mousemove", handleMouseMove);
+        };
+
+        document.addEventListener("mousemove", handleMouseMove);
+        document.addEventListener("mouseup", handleMouseUp, { once: true });
+      },
+    };
+  };
+
+  const calculateWidth = (width: number, deltaX: number) => {
+    if (axis.includes("e")) {
+      return clamp(width + deltaX, minConstraints[0], maxConstraints[0]);
+    }
+
+    if (axis.includes("w")) {
+      return clamp(width - deltaX, minConstraints[0], maxConstraints[0]);
+    }
+
+    return width;
+  };
+
+  const calculateHeight = (height: number, deltaY: number) => {
+    if (axis.includes("n")) {
+      return clamp(height - deltaY, minConstraints[1], maxConstraints[1]);
+    }
+
+    if (axis.includes("s")) {
+      return clamp(height + deltaY, minConstraints[1], maxConstraints[1]);
+    }
+
+    return height;
+  };
+
+  const calculateTop = (bounds: Bounds, deltaY: number) => {
+    if (axis.includes("n")) {
+      return clamp(bounds.top + deltaY, bounds.height + bounds.top - maxConstraints[1], bounds.top + bounds.height - minConstraints[1]);
+    }
+
+    return bounds.top;
+  };
+
+  const calculateLeft = (bounds: Bounds, deltaX: number) => {
+    if (axis.includes("w")) {
+      return clamp(bounds.left + deltaX, bounds.width + bounds.left - maxConstraints[0], bounds.left + bounds.width - minConstraints[0]);
+    }
+
+    return bounds.left;
+  };
+
+  const params = {
+    ...resizeRegister((deltaX, deltaY) => {
+      const nextBounds = {
+        top: calculateTop(bounds, deltaY),
+        left: calculateLeft(bounds, deltaX),
+        width: calculateWidth(bounds.width, deltaX),
+        height: calculateHeight(bounds.height, deltaY),
+      };
+
+      setBounds({ ...nextBounds });
+    }),
+  };
+
+  return <span key={axis} {...params} className={cn(resizeHandleVariants({ axis }))} />;
 }
