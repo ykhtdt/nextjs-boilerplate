d800f0ccda52c38cc7f528012c6203f7445155ca
jgpark-1 <jgpark7281@gmail.com>
2024-02-21 02:12:03 +0900

    chore(eslint): off rules react/no-unescaped-entities

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
-    extends: ["@ykhtdt/eslint-config", "plugin:storybook/recommended"],
-    plugins: ["eslint-plugin-prettier"],
-  };
-  
+  extends: ["@ykhtdt/eslint-config", "plugin:storybook/recommended"],
+  plugins: ["eslint-plugin-prettier"],
+  rules: {
+    "react/no-unescaped-entities": "off",
+  },
+};
