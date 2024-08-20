a82ddde58c166a8ae6b5c1089671c7653e6313c8
jgpark-1 <jgpark7281@gmail.com>
2024-08-20 20:38:09 +0900

    feat: add mobile device detect function

---
+++ src/shared/lib/user-agent.ts
@@ -0,0 +1,16 @@
+"use server"
+
+import { headers } from "next/headers"
+
+import { UAParser } from "ua-parser-js"
+
+export const isMobileDevice = async () => {
+  if (typeof process === "undefined") {
+    throw new Error("[Server method] you are importing a server-only module outside of server")
+  }
+
+  const ua = headers().get("user-agent")
+  const device = new UAParser(ua || "").getDevice()
+
+  return device.type === "mobile"
+}
