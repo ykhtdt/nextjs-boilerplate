84599b3d4ab8a4b664f89d5d4d65b3f9364fbd53
jgpark <jgpark7281@gmail.com>
2024-08-22 12:53:25 +0900

    feat: page props 타입 정의

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -5,3 +5,29 @@ type BaseProps = {
 export type LayoutProps<Params = undefined> = Params extends undefined
   ? BaseProps & { params?: {} }
   : BaseProps & { params: Params };
+
+type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
+
+type UnionToIntersection<T> = Prettify<
+  (T extends any ? (x: T) => any : never) extends (x: infer R) => any
+  ? R
+  : never
+>;
+
+export type PageProps<
+  TParams extends string = never,
+  TSearchParams extends string = never
+> = {
+  params: UnionToIntersection<
+    {
+      [K in TParams]: {
+        [F in K extends `...${infer U}` ? U : K]: K extends `...${string}`
+        ? string[]
+        : string;
+      };
+    }[TParams]
+  >;
+  searchParams: { [K in TSearchParams]?: string | string[] };
+}
