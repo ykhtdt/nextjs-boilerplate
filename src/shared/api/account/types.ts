c7ff1fa932328b1daa76a4909e8eb5dcc3dfe9a4
jgpark-1 <jgpark7281@gmail.com>
2024-10-08 11:24:25 +0900

    chore: v1 project init

--- src/shared/api/account/types.ts
+++
@@ -1,6 +0,0 @@
-export interface User {
-  id: number;
-  email: string;
-  name: string;
-  tel: string;
-}
--- src/shared/lib/utility-types.ts
+++
@@ -1,9 +0,0 @@
-export type Prettify<T> = {
-  [K in keyof T]: T[K];
-} & {};
-
-export type UnionToIntersection<T> = Prettify<
-  (T extends any ? (x: T) => any : never) extends (x: infer R) => any
-  ? R
-  : never
->;
